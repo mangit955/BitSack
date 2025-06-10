ecfa9ddb73b40d594668dfd6365c8e13744ec1a1
Manas Raghuwanshi <manasr955@gmail.com>
2025-06-10 10:13:44 +0530

    Latest

--- src/db.ts
+++ src/db.ts
@@ -17,8 +17,10 @@ export const UserModel = model("User", UserSchema);
 const ContentSchema = new Schema({
     title:String,
     link:String,
+    type: String,
     tags: [{type:mongoose.Types.ObjectId, ref:'Tag'}],
-    userId: {type:mongoose.Types.ObjectId, ref:'User', required:true}
+    userId: {type:mongoose.Types.ObjectId, ref:'User', required:true},
+    shareLink:{type:String, default:null}
 })
 
 export const ContentModel = model("Content", ContentSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express,{Request, Response, }from "express";
+import express,{NextFunction, Request, Response, }from "express";
 import mongoose  from "mongoose";
 import jwt from "jsonwebtoken";
 import { ContentModel, UserModel } from "./db";
 import { JWT_PASSWORD } from "./config";
 import { userMiddleware } from "./middleware";
-import dotenv from "dotenv";
+import dotenv, { populate } from "dotenv";
+import { randomUUID} from "crypto";
+import { link } from "fs";
 
 dotenv.config();
 
@@ -68,10 +70,12 @@ app.post("/api/v1/content",userMiddleware, async (req: AuthRequest, res: Respons
     const link = req.body.link;
     const type = req.body.type;
     await ContentModel.create({
+        title : req.body.title || "Untitled",
         link,
         type,
         userId: req.userId,
-        tags: []
+        tags: [],
+        shareLink: null
     })
 
      res.json({
@@ -80,16 +84,73 @@ app.post("/api/v1/content",userMiddleware, async (req: AuthRequest, res: Respons
     
 })
 
-app.post("/api/v1/content1", (req, res)=> {
+app.get("/api/v1/content",userMiddleware,async (req, res)=> {
+    //@ts-ignore
+    const userId = req.userId;
+    const content = await ContentModel.find({
+        userId: userId
+    }).populate("userId","username")
+    res.json({
+        content
+    })
     
 })
 
-app.post("/api/v1/brain/share", (req, res)=> {
-    
+app.delete("/api/v1/content",userMiddleware,async (req, res)=> {
+
+    const authReq = req as AuthRequest;
+    const contentId = req.body.contentId;
+    const result = await ContentModel.deleteOne({
+        _id: contentId,
+        userId: authReq.userId
+    });
+
+    if (result.deletedCount === 0){
+        res.status(404).json({
+            message: " Content not found"
+        });
+    } else{
+         res.json({
+        message: "Deleted successfully!"
+    })
+    }
 })
 
-app.post("/api/v1/brain/:shareLink", (req, res)=> {
-    
+app.post("/api/v1/brain/content/share",userMiddleware ,async(req, res)=> {
+    const contentId = req.body.contentId;
+    const shareLink = randomUUID();
+    const content = await ContentModel.findByIdAndUpdate(contentId, {shareLink},
+        {new: true}
+    );
+    console.log("Updated content:", content);
+
+    if(!content){
+        res.status(404).json({
+            message: "Content not found"
+        });
+    } else{
+         res.json({
+        message: "Content shared successfully!",
+        shareLink: `${req.protocol}://${req.get("host")}/api/v1/brain/${shareLink}`
+    });  
+
+    }
+});
+
+app.get("/api/v1/brain/:shareLink",async (req, res)=> {
+    const shareLink = req.params.shareLink;
+    console.log("Looking for shared Link:",shareLink)
+    const content = await ContentModel.findOne({ shareLink });
+
+    if(!content){
+        res.status(404).json({
+            message: "Shared content not found"
+        });
+    }else{
+        res.json({
+        link: content.link,
+    });
+    }
 })
 
 app.listen(3000);
